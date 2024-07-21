75f5413092c3703d27401d5882fcdfe5485fce09
AnnadTenneti <tenneti.anand@gmail.com>
2024-07-21 17:41:31 +0530

    Modified testRunner file

--- src/test/java/Runners/testRunner.java
+++ src/test/java/Runners/testRunner.java
@@ -2,30 +2,15 @@ package Runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Parameters;
-import util.ConfigReader;
 
-@CucumberOptions(features = {"src/test/java/features/customer.feature", "src/test/java/features/product.feature"}, //src/test/java/features/customer.feature",
+@CucumberOptions(features = {"src/test/java/features/categories.feature"}, //"src/test/java/features/product.featuresrc/test/java/features/customer.feature",
         glue = "stepdefinitions",
         monochrome = false,
-        //snippets = CucumberOptions.SnippetType.CAMELCASE,
         dryRun = false,
-        tags = "@End2End1 or @Regression1",
-//        plugin = {"pretty", "html:target/test-output.html"},
+        tags = "@CategoriesSO",
         plugin = "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
         publish = true)
 
 public class testRunner extends AbstractTestNGCucumberTests {
-//    @DataProvider(parallel = false)
-//    public Object[][] scenarios() {
-//        return super.scenarios();
-//    }
-//
-//    @BeforeTest
-//    @Parameters({"browser"})
-//    public void defineBrowser(String browser) throws Throwable {
-//        ConfigReader.setBrowserType(browser);
-//    }
+
 }
